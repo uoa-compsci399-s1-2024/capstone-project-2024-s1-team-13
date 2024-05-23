ec7bf1e365a497ca26d1b6bdc158bacfc93c5da9
Saacha Moyo <saachamoyo@gmail.com>
2024-05-23 13:48:01 +1200

    Updated reset_password.dart
    - Changed UI for this screen.

--- lib/welcome/reset_password.dart
+++ lib/welcome/reset_password.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:flutter/widgets.dart';
 
 class PasswordResetPage extends StatefulWidget {
   @override
@@ -87,81 +88,257 @@ class _PasswordResetPageState extends State<PasswordResetPage> {
     }
   }
 
+  Widget _usernameInput() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Username",
+            style: TextStyle(
+              fontFamily: "Lexend Exa",
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextField(
+              controller: _usernameController,
+              style: const TextStyle(
+                  fontFamily: "Lexend Exa",
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500),
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 25.0),
+                filled: true,
+                fillColor: Colors.grey[300],
+                hintText: "Enter username",
+                hintStyle: TextStyle(
+                    fontFamily: "Lexend Exa",
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[400]),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none),
+              )),
+        ],
+      );
+
+  Widget _verificationCodeInput() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Verification Code",
+            style: TextStyle(
+              fontFamily: "Lexend Exa",
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextField(
+              controller: _verificationCodeController,
+              style: const TextStyle(
+                  fontFamily: "Lexend Exa",
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500),
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 25.0),
+                filled: true,
+                fillColor: Colors.grey[300],
+                hintText: "Enter verification code",
+                hintStyle: TextStyle(
+                    fontFamily: "Lexend Exa",
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[400]),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none),
+              )),
+        ],
+      );
+
+  Widget _newPasswordCodeInput() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "New Password",
+            style: TextStyle(
+              fontFamily: "Lexend Exa",
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextField(
+            controller: _newPasswordController,
+            style: const TextStyle(
+                fontFamily: "Lexend Exa",
+                fontSize: 20,
+                fontWeight: FontWeight.w500),
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+              suffixIcon: IconButton(
+                icon: Icon(
+                    _isNewPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    size: 30),
+                onPressed: () {
+                  setState(() {
+                    _isNewPasswordVisible = !_isNewPasswordVisible;
+                  });
+                },
+              ),
+              filled: true,
+              fillColor: Colors.grey[300],
+              hintText: "Enter new password",
+              hintStyle: TextStyle(
+                  fontFamily: "Lexend Exa",
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[400]),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  borderSide: BorderSide.none),
+            ),
+            obscureText: !_isNewPasswordVisible,
+            onChanged: _validatePassword,
+          ),
+        ],
+      );
+
+  Widget _confirmPasswordCodeInput() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Confirm Password",
+            style: TextStyle(
+              fontFamily: "Lexend Exa",
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextField(
+            controller: _confirmPasswordController,
+            style: const TextStyle(
+                fontFamily: "Lexend Exa",
+                fontSize: 20,
+                fontWeight: FontWeight.w500),
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+              suffixIcon: IconButton(
+                icon: Icon(
+                    _isConfirmPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    size: 30),
+                onPressed: () {
+                  setState(() {
+                    _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                  });
+                },
+              ),
+              filled: true,
+              fillColor: Colors.grey[300],
+              hintText: "Enter password again",
+              hintStyle: TextStyle(
+                  fontFamily: "Lexend Exa",
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[400]),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  borderSide: BorderSide.none),
+            ),
+            obscureText: !_isConfirmPasswordVisible,
+            onChanged: _validatePassword,
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Reset Password'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
-            ),
-            if (_isCodeSent) ...[
-              TextField(
-                controller: _verificationCodeController,
-                decoration: InputDecoration(labelText: 'Verification Code'),
+          title: Text('Reset Password'),
+          leading: IconButton(
+              iconSize: 40,
+              icon: const Icon(Icons.arrow_back_ios),
+              padding:
+                  const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 10.0),
+              onPressed: () {
+                Navigator.pop(context);
+              })),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20, left: 50, right: 50),
+                child: _usernameInput(),
               ),
-              TextField(
-                controller: _newPasswordController,
-                decoration: InputDecoration(
-                  labelText: 'New Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isNewPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+              SizedBox(height: 15),
+              if (_isCodeSent) ...[
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: _verificationCodeInput(),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: _newPasswordCodeInput(),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                    padding: EdgeInsets.only(left: 50, right: 50),
+                    child: _confirmPasswordCodeInput()),
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: _buildPasswordValidationIndicators(),
+                ),
+              ],
+              SizedBox(height: 25),
+              ElevatedButton(
+                  onPressed:
+                      _isCodeSent ? _resetPassword : _sendVerificationCode,
+                  child: Text(_isCodeSent
+                      ? 'Reset Password'
+                      : 'Send Verification Code'),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(500, 80),
+                      foregroundColor: Colors.white,
+                      textStyle: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'Lexend Exa',
+                        fontWeight: FontWeight.w500,
+                      ),
+                      backgroundColor: Colors.pink[900],
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)))),
+              if (_errorMessage.isNotEmpty) ...[
+                SizedBox(height: 10),
+                Expanded(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 50, right: 50, top: 10),
+                    child: Text(
+                      _errorMessage,
+                      style: TextStyle(
+                          fontFamily: 'Lexend Exa',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.red),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isNewPasswordVisible = !_isNewPasswordVisible;
-                      });
-                    },
                   ),
                 ),
-                obscureText: !_isNewPasswordVisible,
-                onChanged: _validatePassword,
-              ),
-              TextField(
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(
-                  labelText: 'Confirm Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isConfirmPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
-                      });
-                    },
-                  ),
-                ),
-                obscureText: !_isConfirmPasswordVisible,
-                onChanged: _validateConfirmPassword,
-              ),
-              _buildPasswordValidationIndicators(),
+              ],
             ],
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _isCodeSent ? _resetPassword : _sendVerificationCode,
-              child: Text(
-                  _isCodeSent ? 'Reset Password' : 'Send Verification Code'),
-            ),
-            if (_errorMessage.isNotEmpty) ...[
-              SizedBox(height: 20),
-              Text(
-                _errorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            ],
-          ],
+          ),
         ),
       ),
     );
@@ -187,9 +364,16 @@ class _PasswordResetPageState extends State<PasswordResetPage> {
         Icon(
           isValid ? Icons.check : Icons.close,
           color: isValid ? Colors.green : Colors.red,
+          size: 30,
         ),
-        SizedBox(width: 8),
-        Text(text),
+        SizedBox(width: 10),
+        Text(
+          text,
+          style: TextStyle(
+              fontFamily: 'Lexend Exa',
+              fontSize: 20,
+              fontWeight: FontWeight.w500),
+        ),
       ],
     );
   }
