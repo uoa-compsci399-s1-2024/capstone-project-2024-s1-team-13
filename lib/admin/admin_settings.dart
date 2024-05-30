93e6c65178c7bd5a189a2b7bcd755658e80445ae
Saacha Moyo <saachamoyo@gmail.com>
2024-05-30 13:30:38 +1200

    Removed change_login.dart
    -  Removed this screen.
    - Minor UI changes to settings screen.

--- lib/admin/admin_settings.dart
+++ lib/admin/admin_settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:inka_test/admin/admin_change_login.dart';
 import 'package:inka_test/welcome/inka_welcome.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 
@@ -21,96 +20,76 @@ class AdminSettings extends StatelessWidget {
             }),
       ),
       body: Center(
-          child: Column(children: [
-        const SizedBox(height: 100),
+          child: Padding(
+        padding:
+            const EdgeInsets.only(left: 50, right: 50, top: 100, bottom: 100),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Support Staff Avatar
+              Column(
+                children: [
+                  Container(
+                      width: 300,
+                      height: 300,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(255, 196, 155, 175),
+                              Color.fromARGB(255, 87, 195, 245)
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          )),
+                      child: const Center(
+                          child: Text(
+                        'A',
+                        style: TextStyle(
+                            fontFamily: 'Lexend Exa',
+                            fontSize: 100,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white),
+                      ))),
+                  const SizedBox(height: 20),
+                  // Text
+                  Text("Admin",
+                      style: TextStyle(
+                          fontFamily: 'Lexend Exa',
+                          fontSize: 40,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.pink[900])),
+                ],
+              ),
 
-        // Support Staff Avatar
-        Container(
-            width: 300,
-            height: 300,
-            decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromARGB(255, 196, 155, 175),
-                    Color.fromARGB(255, 87, 195, 245)
-                  ], // Gradient colors
-                  begin: Alignment.topLeft, // Gradient begin position
-                  end: Alignment.bottomRight, // Gradient end position
-                )),
-            child: const Center(
-                child: Text(
-              'A',
-              style: TextStyle(
-                  fontFamily: 'Lexend Exa',
-                  fontSize: 100,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white),
-            ))),
-
-        const SizedBox(height: 20),
-
-        // Text
-        Text("Admin",
-            style: TextStyle(
-                fontFamily: 'Lexend Exa',
-                fontSize: 40,
-                fontWeight: FontWeight.w500,
-                color: Colors.pink[900])),
-
-        const SizedBox(height: 200),
-
-        // Change Login Button
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const AdminChangeLogin(
-                    title: 'Change Login'); // To change
-              }));
-            },
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(700, 100),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'Lexend Exa',
-                  fontWeight: FontWeight.w500,
-                ),
-                backgroundColor: Colors.pink[900],
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50))),
-            child: const Text("Change Login")),
-
-        const SizedBox(height: 20),
-
-        // Logout Button - pending backend functionality
-        ElevatedButton(
-            onPressed: () async {
-              try {
-                await Amplify.Auth.signOut();
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) {
-                  return InkaWelcome(title: 'Welcome');
-                }));
-              } catch (e) {
-                print('Error signing out: $e');
-              }
-            },
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(700, 100),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'Lexend Exa',
-                  fontWeight: FontWeight.w500,
-                ),
-                backgroundColor: Colors.pink[900],
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50))),
-            child: const Text("Logout"))
-      ])),
+              // Logout Button
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await Amplify.Auth.signOut();
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) {
+                        return InkaWelcome(title: 'Welcome');
+                      }));
+                    } catch (e) {
+                      print('Error signing out: $e');
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(700, 100),
+                      foregroundColor: Colors.white,
+                      textStyle: const TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Lexend Exa',
+                        fontWeight: FontWeight.w500,
+                      ),
+                      backgroundColor: Colors.pink[900],
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                  child: const Text("Logout"))
+            ]),
+      )),
     );
   }
 }
